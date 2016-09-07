2fff268bf8b1d54af1294f544cf53bec6a1fd120
Kate Demchuk <kate.v.demchuk@gmail.com>
2016-09-07 05:01:08 +0300

    Cleared dist folder, checked dev version files.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,11 @@ var gulp = require('gulp'),
 		sass = require('gulp-ruby-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
 		mustache = require('gulp-mustache-plus'),
-		htmlmin = require('gulp-htmlmin');
+		htmlmin = require('gulp-htmlmin'),
+		uglify = require('gulp-uglify'),
+		clean = require('gulp-clean');
 
+// *****************SERVER***************** //
 gulp.task('connect', function() {
 	connect.server({
 		port: 1337,
@@ -15,6 +18,7 @@ gulp.task('connect', function() {
 	});
 });
 
+// *****************STYLES***************** //
 gulp.task('sass', function() {
 	return sass('./sass/*.*', {
 		style: 'compressed'
@@ -22,26 +26,15 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('mustache', function() {
-	gulp.src('./templates/*.mustache')
-		.pipe(mustache({
-			// siteURL: 'http://localhost:1337'
-			siteURL: 'http://ascentlight.com'
-		}, {}, {
-			head: './templates/layout/head.mustache',
-			nav: './templates/layout/nav.mustache',
-			mobilenav: './templates/layout/mobilenav.mustache',
-			footer: './templates/layout/footer.mustache'
+gulp.task('sass-media', function() {
+	return sass('./sass/media/*.sass', {
+		style: 'compressed'
+	})
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
 		}))
-		.pipe(gulp.dest('./dist/html/'));
-});
-
-gulp.task('htmlmin', function() {
-	return gulp.src('./dist/html/*.html')
-		.pipe(htmlmin({
-			collapseWhitespace: true
-		}))
-		.pipe(gulp.dest('./dist/'));
+		.pipe(gulp.dest('./dist/css/media'));
 });
 
 gulp.task('concat', function() {
@@ -62,24 +55,66 @@ gulp.task('concat', function() {
 		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('sass-media', function() {
-	return sass('./sass/media/*.sass', {
-		style: 'compressed'
-	})
+gulp.task('concat-media', function() {
+	return gulp.src('./dist/css/media/*.css')
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
+		.pipe(concat('media.min.css'))
 		.pipe(gulp.dest('./dist/'));
 });
 
+// *****************TEMPLATES***************** //
+gulp.task('mustache', function() {
+	gulp.src('./templates/*.mustache')
+		.pipe(mustache({
+			// siteURL: 'http://localhost:1337'
+			siteURL: 'http://ascentlight.com',
+			formEmail: 'https://formspree.io/your@email.com'
+		}, {}, {
+			head: './templates/layout/head.mustache',
+			nav: './templates/layout/nav.mustache',
+			mobilenav: './templates/layout/mobilenav.mustache',
+			footer: './templates/layout/footer.mustache'
+		}))
+		.pipe(gulp.dest('./dist/html/'));
+});
+
+gulp.task('htmlmin', function() {
+	return gulp.src('./dist/html/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest('./dist/'));
+});
+
+// *****************SCRIPTS***************** //
+gulp.task('scripts', function() {
+	return gulp.src('./js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js/'));
+});
+
+// *****************WATCH***************** //
 gulp.task('watch', function() {
 	gulp.watch('./sass/*.sass', ['sass']);
 	gulp.watch('./sass/media/*.sass', ['sass-media']);
 	gulp.watch('./dist/css/*.css', ['concat']);
+	gulp.watch('./dist/css/media/*.css', ['concat-media']);
 	gulp.watch('./templates/*.mustache', ['mustache', 'htmlmin']);
-	gulp.watch('./templates/*/*.mustache', ['mustache', 'htmlmin']);
+	gulp.watch('./templates/layout/*.mustache', ['mustache']);
 	gulp.watch('./dist/html/*.html', ['htmlmin']);
 });
 
-gulp.task('default', ['connect', 'sass', 'concat', 'sass-media', 'mustache', 'htmlmin', 'watch']);
+// *****************CLEAN***************** //
+gulp.task('clean', function() {
+	return gulp.src([
+			'./dist/css',
+			'./dist/html'
+		])
+		.pipe(clean());
+});
+
+// *****************DEFAULT***************** //
+gulp.task('default', ['connect', 'sass', 'sass-media', 'concat', 'concat-media', 'mustache', 'htmlmin', 'scripts', 'watch']);
